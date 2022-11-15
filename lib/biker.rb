32edb57f1be5acd96d74780206ef04b26c96165c
Brian Hayes <Brian.hayes1200@gmail.com>
2022-11-15 11:57:00 -0700

    incomplete

--- lib/biker.rb
+++ lib/biker.rb
@@ -14,4 +14,10 @@ class Biker
   def learn_terrain!(terrain)
     @acceptable_terrain << terrain
   end 
+
+  def log_ride(ride, distance)
+      rides[ride] = []
+      rides[ride] << distance
+    
+  end 
 end 
